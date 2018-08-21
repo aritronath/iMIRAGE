5cecba4f35b913ca972bc6be225163cfa1b7aada
Aritro Nath <aritronath@gmail.com>
2018-08-21 12:54:42 +0530

    rf ntree = 250

--- R/iMIRAGE.R
+++ R/iMIRAGE.R
@@ -158,7 +158,7 @@ imirage_cv <- function (train_pcg, train_mir, gene_index, num=50, method, folds=
 
     if(method=="RF") {
       #randomForest
-      imp.rf <- randomForest(x, y, ntree=500)
+      imp.rf <- randomForest(x, y, ntree=250)
       predict.y <- predict(imp.rf, testx)
       r.rf <- cor.test(predict.y, actual.y, method="spearman")
       rmse.rf <- sqrt(mean((actual.y-predict.y)^2))
@@ -215,7 +215,7 @@ imirage <- function (train_pcg, train_mir, my_pcg, gene_index, method, num=50) {
   if (ncol(train_pcg) < 50) x <- scale(train_pcg)
   
   if (method=="RF") {
-    rfit <- randomForest(x, y, ntree=500)
+    rfit <- randomForest(x, y, ntree=250)
     predict.y <-  predict(rfit, my_pcg)
     return(predict.y)
   }
