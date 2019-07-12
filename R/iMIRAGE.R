a3954f7882c1ff17c7d6c05dddeb1d47e571375b
Aritro Nath <nath0050@ad.ahc.umn.edu>
2019-07-12 16:05:13 -0500

    Version 1.1 Base

--- R/iMIRAGE.R
+++ R/iMIRAGE.R
@@ -1,44 +1,44 @@
-#' @title Pre-processing gene expression matrix 
-#' 
-#' @description Use this function to perform a number of pre-processing steps, including 
-#' log transformation, normalization and standardization of gene expression matrix. 
+#' @title Pre-processing gene expression matrix
+#'
+#' @description Use this function to perform a number of pre-processing steps, including
+#' log transformation, normalization and standardization of gene expression matrix.
 #' NOTE: If multiple options are set true, the sequence of pre-processing steps
-#' performed are variance filtering, log2 transformation, upper quantile normalization 
-#' and standardization. 
-#' 
-#' @param gex a gene expression matrix with genes in columns and samples in rows 
+#' performed are variance filtering, log2 transformation, upper quantile normalization
+#' and standardization.
+#'
+#' @param gex a gene expression matrix with genes in columns and samples in rows
 #' @param var.filter logical. specify whether genes are filtered based on variance. Default = TRUE
-#' @param log logical. specify whether log2(x+1) transformation should be performed. Default = FALSE 
+#' @param log logical. specify whether log2(x+1) transformation should be performed. Default = FALSE
 #' @param UQ logical. specify whether upper quantile normalization should be performed. Default = FALSE
 #' @param scale logical. specifiy whether standardization should be performed. Default = TRUE
 #'
 #' @return a processed gene expression matrix
-#' 
+#'
 #' @examples
 #' train_pcg <- pre.process(train_pcg)
-#' 
+#'
 #' @export
 pre.process <- function (gex, var.filter=TRUE, log=FALSE, UQ=FALSE, std=TRUE) {
   if (mode(gex)!="numeric" | class(gex)!="matrix") stop ("Error: input data must be a numeric matrix")
-  
+
   if (var.filter == TRUE) {
     gvar <- apply(gex, 2, var)
     gex <- gex[, which(gvar!=0)]
   }
-  
+
   if(log == TRUE) gex <- log2(gex + 1)
-  
+
   if (UQ == TRUE) gex <- apply(gex, 2, function (x) x/quantile(x, 0.75))
- 
+
   if (std == TRUE) gex <- scale(gex)
-  
+
   return(gex)
 }
 
 
-#' @title Pre-standardization filter to select miRNA expressed in a specific proportion of samples 
+#' @title Pre-standardization filter to select miRNA expressed in a specific proportion of samples
 #'
-#' @description return a subset of the miRNA expression dataset containing miRNAs that are expressed 
+#' @description return a subset of the miRNA expression dataset containing miRNAs that are expressed
 #' in at least a specified percentage of samples based on a user-defined expression threshold
 #'
 #' @param train_mir an expression matrix with miRNA in columns, samples in rows
@@ -46,9 +46,9 @@ pre.process <- function (gex, var.filter=TRUE, log=FALSE, UQ=FALSE, std=TRUE) {
 #' @param threshold the numeric threshold defining expression of miRNA. Default threshold = 0.
 #' @return filtered miRNA expression matrix
 #'
-#' @examples 
+#' @examples
 #' filter.exp(train_mir, cutoff=75, threshold=0)
-#' 
+#'
 #' @export
 filter.exp <- function (train_mir, cutoff=75, threshold=0) {
 
@@ -72,18 +72,18 @@ filter.exp <- function (train_mir, cutoff=75, threshold=0) {
 
 #' @title Match gene expression matrices by gene ID
 #'
-#' @description Returns expression matrices with common genes 
+#' @description Returns expression matrices with common genes
 #'
-#' @param train_pcg Gene expression matric with gene IDs as column names 
+#' @param train_pcg Gene expression matric with gene IDs as column names
 #' @param my_pcg Gene expression matric with gene IDs as column names
 #'
 #' @return A list containing two matrices with common genes
 #'
-#' @examples 
+#' @examples
 #' temp <- match.gex(train_pcg, my_pcg)
 #' train_pcg <- data.matrix(temp[[1]])
 #' my_pcg <- data.matrix(temp[[2]])
-#' 
+#'
 #' @export
 match.gex <- function (train_pcg, my_pcg) {
 
@@ -105,34 +105,34 @@ match.gex <- function (train_pcg, my_pcg) {
   return(cmat)
 }
 
-#' @title Filter training gene expression features  
+#' @title Filter training gene expression features
 #'
-#' @description Function to filter informative protein coding genes based on correlation with 
+#' @description Function to filter informative protein coding genes based on correlation with
 #' miRNA expression
-#' 
-#' @param train_pcg training protein coding expression dataset. a numeric matrix with row names 
+#'
+#' @param train_pcg training protein coding expression dataset. a numeric matrix with row names
 #' indicating samples, and column names indicating protein coding gene IDs.
-#' @param train_mir training miRNA expression dataset. a numeric matrix with row names indicating 
+#' @param train_mir training miRNA expression dataset. a numeric matrix with row names indicating
 #' samples, and column names indicating miRNA IDs
 #' @param gene_index either gene name (character) or index (column number) of the miRNA to be imputed.
-#' @param num number of informative protein coding genes to be used in constructing imputation model. 
+#' @param num number of informative protein coding genes to be used in constructing imputation model.
 #' Default is 50 genes.
-#' @param target logical, specifying whether protein coding genes should be restricted to predicted 
-#' targets of the miRNA (from TargetScan) or use all genes as candidates. Default = FALSE.  
+#' @param target logical, specifying whether protein coding genes should be restricted to predicted
+#' targets of the miRNA (from TargetScan) or use all genes as candidates. Default = FALSE.
 #'
 #' @return a numeric matrix. subset of protein coding genes correlated with miRNA of interest.
 corf <- function (train_pcg, train_mir, gene_index, num=50, target=FALSE) {
   if (target==TRUE) {
-    m1 <- grep(colnames(train_mir)[gene_index], ts.pairs$miRNA) #get all entries in the ts.pair table 
-    
-    if (sum(!is.na(m1))==0) { 
+    m1 <- grep(colnames(train_mir)[gene_index], ts.pairs$miRNA) #get all entries in the ts.pair table
+
+    if (sum(!is.na(m1))==0) {
       pcor <- abs(cor(train_pcg, train_mir[, gene_index]))
       r_pcor <- rank(-pcor)
       gin <- which (r_pcor < num)
       temp_pcg <- train_pcg[, gin]
       warning ("miRNA not found in target-pair table. Using all genes")
     }
-    
+
     if (sum(!is.na(m1)) > 0) {
       m2 <- match(ts.pairs$GeneID[m1], colnames(train_pcg))  #get target IDs
       temp <- train_pcg[, na.omit(m2)]
@@ -149,37 +149,37 @@ corf <- function (train_pcg, train_mir, gene_index, num=50, target=FALSE) {
     gin <- which (r_pcor < num)
     temp_pcg <- train_pcg[, gin]
   }
-  
+
   return(temp_pcg)
 }
 
 
 #' @title Cross validation function for iMIRAGE imputation accuracy
 #'
-#' @description Function to obtain accuracy parameters: correlation coefficient, P-value and RMSE of 
+#' @description Function to obtain accuracy parameters: correlation coefficient, P-value and RMSE of
 #' imputation model
 #'
-#' @param train_pcg training protein coding dataset. a numeric matrix with row names indicating 
+#' @param train_pcg training protein coding dataset. a numeric matrix with row names indicating
 #' samples, anSed column names indicating protein coding gene IDs.
-#' @param train_mir training miRNA expression dataset. a numeric matrix with row names indicating 
+#' @param train_mir training miRNA expression dataset. a numeric matrix with row names indicating
 #' samples, and column names indicating miRNA IDs
 #' @param gene_index either gene name (character) or index (column number) of miRNA to be imputed.
-#' @param method method for imputation, either RF or KNN, for random forests and K-nearest neighbor 
+#' @param method method for imputation, either RF or KNN, for random forests and K-nearest neighbor
 #' respectively.
-#' @param num number of informative protein coding genes to be used in constructing imputation model. 
+#' @param num number of informative protein coding genes to be used in constructing imputation model.
 #' Default is 50 genes.
-#' @param folds number specifying folds (k) of cross validation to obtain imputation accuracy. 
+#' @param folds number specifying folds (k) of cross validation to obtain imputation accuracy.
 #' Default is k=10.
-#' @param target logical, specifying whether protein coding genes should be restricted to predicted 
-#' targets of the miRNA (from TargetScan) or use all genes as candidates. Default = FALSE. 
+#' @param target logical, specifying whether protein coding genes should be restricted to predicted
+#' targets of the miRNA (from TargetScan) or use all genes as candidates. Default = FALSE.
 #'
 #' @return a matrix with three values corresponding to Spearman's correlation coefficient,
 #' P-value of the fit and root mean squared error (RMSE).
 #'
-#' @examples 
+#' @examples
 #' imirage_cv(train_pcg, train_mir, gene_index="ENSG00000184441", method="RF", num=100)
 #' imirage_cv(train_pcg, train_mir, gene_index=25, method="RF", num=100)
-#' 
+#'
 #' @import randomForest
 #' @import FNN
 #' @export
@@ -194,7 +194,7 @@ imirage.cv <- function (train_pcg, train_mir, gene_index, num=50, method, folds=
 
   if (num >= 50 & ncol(train_pcg) >=50 ) x <- scale (corf(train_pcg, train_mir, gene_index, num, target))
   if (ncol(train_pcg) < 50) x <- scale(train_pcg)
-  
+
   if (mode(gene_index)=="numeric") {
     if (num >= 50 & ncol(train_pcg) >=50 ) train_pcg <- scale(corf(train_pcg, train_mir, gene_index, num, target))
     if (ncol(train_pcg) < 50) train_pcg <- scale(train_pcg)
@@ -203,7 +203,7 @@ imirage.cv <- function (train_pcg, train_mir, gene_index, num=50, method, folds=
     if (num >= 50 & ncol(train_pcg) >=50 ) train_pcg <- scale(corf(train_pcg, train_mir, gene_index, num, target))
     if (ncol(train_pcg) < 50) train_pcg <- scale(train_pcg)
   }
-  
+
   train_mir <- scale(train_mir)
 
   cat("\nRunning ", folds,"-folds cross-validation...", sep="")
@@ -235,9 +235,9 @@ imirage.cv <- function (train_pcg, train_mir, gene_index, num=50, method, folds=
       rmse.rf <- sqrt(mean((actual.y-predict.y)^2))
       cv.res [k, 1:3] <- c(r.rf$estimate, r.rf$p.value, rmse.rf)
     }
-    
+
     if (method=="KNN") {
-      #knn regression 
+      #knn regression
       knn.fit <- knn.reg(train = x, test = testx, y = y, k=50)
       r.knn <- cor.test(knn.fit$pred, actual.y, method="spearman")
       rmse.knn <- sqrt(mean((actual.y - knn.fit$pred)^2))
@@ -254,24 +254,24 @@ imirage.cv <- function (train_pcg, train_mir, gene_index, num=50, method, folds=
 #'
 #' @param train_pcg training protein coding dataset. a numeric matrix with with row names indicating
 #' samples, and column names indicating protein coding gene IDs.
-#' @param train_mir training miRNA expression dataset. a numeric matrix with row names indicating 
+#' @param train_mir training miRNA expression dataset. a numeric matrix with row names indicating
 #' samples, and column names indicating miRNA IDs.
-#' @param my_pcg test protein coding expression dataset. a numeric matrix with row names indicating 
+#' @param my_pcg test protein coding expression dataset. a numeric matrix with row names indicating
 #' samples, and column names indicating protein coding gene IDs.
 #' @param gene_index either gene name (character) or index (column number) of miRNA to be imputed.
-#' @param method method for imputation, either RF or KNN, for random forests and K-nearest neighbor 
+#' @param method method for imputation, either RF or KNN, for random forests and K-nearest neighbor
 #' respectively.
-#' @param num number of informative protein coding genes to be used in constructing imputation model. 
+#' @param num number of informative protein coding genes to be used in constructing imputation model.
 #' Default is 50 genes.
-#' @param target logical, specifying whether protein coding genes should be restricted to predicted 
-#' targets of the miRNA (from TargetScan) or use all genes as candidates. Default = FALSE. 
+#' @param target logical, specifying whether protein coding genes should be restricted to predicted
+#' targets of the miRNA (from TargetScan) or use all genes as candidates. Default = FALSE.
 #'
 #' @return imputed expression levels of the miRNA.
 #'
-#' @examples 
+#' @examples
 #' imirage(train_pcg, train_mir, my_pcg, gene_index="ENSG00000228630", method="KNN", num=50)
 #' imirage(train_pcg, train_mir, my_pcg, gene_index=25, method="KNN", num=50)
-#' 
+#'
 #' @import randomForest
 #' @import FNN
 #' @export
@@ -290,13 +290,13 @@ imirage <- function (train_pcg, train_mir, my_pcg, gene_index, method, num=50, t
 
   if (num >= 50 & ncol(train_pcg) >=50 ) x <- scale (corf(train_pcg, train_mir, gene_index, num, target))
   if (ncol(train_pcg) < 50) x <- scale(train_pcg)
-  
+
   if (method=="RF") {
     rfit <- randomForest(x, y, ntree=250)
     predict.y <-  predict(rfit, my_pcg)
     return(predict.y)
   }
-  
+
   if (method=="KNN") {
     knn.fit <- knn.reg(train = x, y = y, k=50)
     return(knn.fit$pred)
